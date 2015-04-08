9979385eca6b6bd99006d6c7dd77445e4cdaaacf
luca-taina <luca.taina@libero.it>
2015-04-08 21:42:40 +0200

    Update

--- src/ImageProcessingMPI.c
+++ src/ImageProcessingMPI.c
@@ -4,7 +4,11 @@
  Author      : Manca-Nero-Taina
  Version     :
  Copyright   : 
- Description : Calculate Pi in MPI
+ Description : Use MPI (+OpenMP) to implement a parallel version of the gamma correction image enhancement 
+               algorithm for grayscale images. Use the image format you prefer ("pgm - portable graymap" 
+               is an easy to parse grayscale format). The code has to be demonstrated using at least two 
+               physical machines connected in a LAN (with or without additional virtual machines to emulate 
+               a larger cluster). 
  ============================================================================
  */
 #include <mpi.h>
