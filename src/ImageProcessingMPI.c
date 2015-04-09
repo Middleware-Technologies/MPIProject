e26f50961489f75d94028faa4a50e2b7d92983df
luca-taina <luca.taina@libero.it>
2015-04-09 15:56:02 +0200

    Aggiunti comandi pgm nel programma MPI - funzionante

--- src/ImageProcessingMPI.c
+++ src/ImageProcessingMPI.c
@@ -1,5 +1,4 @@
 /*
- ============================================================================
  Name        : ImageProcessingMPI.c
  Author      : Manca-Nero-Taina
  Description : Use MPI (+OpenMP) to implement a parallel version of the gamma correction image enhancement 
@@ -9,15 +8,57 @@
                a larger cluster). 
 
 ##COMPILAZIONE DA TERMINALE
-mpicc OUT -o FILE
+mpicc src/ImageProcessingMPI.c -o out/OUT -I /usr/local/netpbm/include/ -L /usr/local/netpbm/lib -lnetpbm -lm
 
 ##ESECUZIONE MULTITHREADING DA TERMINALE
-PATH/mpirun -nc NUM OUT
+PATH/mpirun -np NUM OUT
  */
 
 #include <mpi.h>
 #include <stdio.h>
 #include <string.h>
+#include <pgm.h>
+
+/*
+  Open a MPG file and re-write a new file with different color intensity
+ */
+void processPGM(int argc, char *argv[])
+{
+    //2d array that contains image's data
+    gray **image;
+
+    //Maximum value of our input image, probably
+    gray max;
+
+    //Num cols,row,matrix's indices
+    int cols, rows,y,x;
+
+    //Initialize libpgm
+    pgm_init(&argc, argv);
+   
+    //Read the image
+    FILE *f=fopen("../img/imm.pgm","r");
+    image = pgm_readpgm(f, &cols, &rows, &max);
+
+    for (y=0; y<rows; y++)
+      {
+        for (x=0; x<cols; x++)
+	  {
+            image[y][x] = image[y][x]/2;
+	    printf("%d ",image[y][x]);
+	  }
+	printf("\n");
+      }
+
+    //Write the modified image to another file */
+    FILE *fout=fopen("../img/imm2.pgm","w");
+    pgm_writepgm(fout, image, cols, rows, max, 1);
+
+
+    /* cleanup */
+    pgm_freearray(image, rows);
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -38,31 +79,25 @@ int main(int argc, char *argv[])
 
 	if (my_rank == 0) //IF I'M MASTER THREAD
 	{
-
-
 		//(SEND PART OF IMAGE TO OTHER THREAD)
 		//1 - READ MY PORTION OF IMAGE
 		//2 - PROCESS IMAGE WITH OPENMPFOR INCREMENT PERFORMANCE
 		//3 - ATTENDS PART OF IMAGE FROM OTHER THREAD
 		//4 - CREATE FINAL IMAGE
 
-
+	  processPGM(argc,argv);
 	}
 	else //IF I'M ANOTHER THREAD
 	{
-
-
 		//1 - READ MY PORTION OF IMAGE/RECEIVE PORTION FROM MASTER
 		//1 - READ MY PORTION OF IMAGE
 		//2 - PROCESS IMAGE WITH OPENMPFOR INCREMENT PERFORMANCE
 		//3 - SEND FINAL PART OF IMAGE TO MASTER THREAD
-
-
 	}
-
-
 
 	/* shut down MPI */
 	MPI_Finalize(); 
 	return 0;
 }
+
+
