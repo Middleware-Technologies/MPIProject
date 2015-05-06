fde9307cfcb96515b9c237932d915cc4ce810422
luca.taina <luca.taina@libero.it>
2015-05-06 16:23:38 +0200

    Ripristinato vecchio codice

--- src/ImageProcessingMPI.c
+++ src/ImageProcessingMPI.c
@@ -24,15 +24,14 @@ PATH/mpirun -host LIST OUT
 #include <mpi.h>
 #include <stdio.h>
 #include <string.h>
-#include <pam.h>
+#include <pgm.h>
 #include <math.h>
 
 
 int applyCorrection(int input, double gamma, int depth)
 {
-	double pot=1/((double)gamma);
 	double base=(double)input/(double)depth;
-	double val= depth* pow(base,pot);
+	double val= depth* pow(base,1/gamma);
 	return (int)val;
 }
 
@@ -51,82 +50,58 @@ int main(int argc, char *argv[])
 	printf("Process: %d - Total Process: %d\n", my_rank, num_procs);
 
 	if (my_rank == 0) {		// IF I'M MASTER THREAD
-	        // The two pam, one for input and one for output,structures
-	        // containing infos about the image
-	        struct pam inpam, outpam;
-
-	        // The 3D array containing the values
-	        tuple ** imageArray;
-
-	        // Indexes to browse the array
-	        unsigned int row, column, plane;
+	        //Data Structure
+	        gray **imageArray;
+		gray max;      
+	        unsigned int row, column,x,y;
 
 		//0 - READ FILE'S MATRIX
 		pgm_init(&argc, argv);
 
 		// Read the input image and pass
-		inpam.file = fopen("img/imm.pgm", "r");
-		imageArray = pnm_readpam(inpam.file, &inpam, PAM_STRUCT_SIZE(tuple_type));
-		
-		int imageIntArray[inpam.height][inpam.width];
+		FILE *f = fopen("img/imm.pgm", "r");
+		imageArray = pgm_readpgm(f, &column, &row, &max);
 		
 		//TEST: Print Matrix
 		printf("Thread 0 Read:\n");
-		for (row = 0; row < inpam.height; row++) {
-			for (column = 0; column < inpam.width; column++) {
-				for (plane = 0; plane < inpam.depth; ++plane) {
-				  imageIntArray[row][column]=(int)imageArray[row][column][plane];
-				  if(imageIntArray[row][column]<10)
-				    printf("%d  ",imageIntArray[row][column]);
-				  else
-				    printf("%d ",imageIntArray[row][column]);
-				}
+		for (x = 0; x < row; x++) {
+			for (y = 0; y < column; y++) {				  				 
+			    if(imageArray[x][y]<10)
+			      printf("%d  ",imageArray[x][y]);
+			    else
+			      printf("%d ",imageArray[x][y]);		  
 			}
 			printf("\n");
 		}
 		printf("\n");
 
-		//1 - SEND PORTION OF THE MATRIX TO OTHER THREADS  - TEST WITH 2 PROCESS
-		int index;   
-		int numRows;   
+		//1 - SEND PORTION OF THE MATRIX TO OTHER THREADS - TEST WITH 2 PROCESS
+		int index;     
 		
 		//How many rows we need to send to every process?
-		if(inpam.height%2==0)
-		  numRows=inpam.height/2;
-		else
-		  numRows=(inpam.height-1)/2;
 		  
 		for(index=1;index<num_procs;index++) 
 		{
 			//Create a packet to send to index thread
-			int param[3]={inpam.height-numRows,inpam.width,(int)inpam.maxval};
-
 			//First: send packet's dimension {numRowsSend,numColsSend,depth}
-			MPI_Send(param,3,MPI_INT,index,1,MPI_COMM_WORLD);
+			int param[3]={3,column,(int)max};
+			int numRow=3;
+			MPI_Send(param,numRow,MPI_INT,index,1,MPI_COMM_WORLD);
 
 			//Send Packet
-			int dimension=(inpam.height-numRows)*inpam.width;
-			MPI_Send(&imageIntArray[numRows][0],dimension,MPI_INT,index,2,MPI_COMM_WORLD);
+			int dimension=numRow*column;
+			MPI_Send(&imageArray[0][0],dimension,MPI_INT,index,2,MPI_COMM_WORLD);
 		}
 
 		//2 - PROCESS IMAGE WITH OPENMP FOR INCREMENT PERFORMANCE
 
-		//3 - ATTENDS PART OF IMAGE FROM OTHER THREAD
+		//3 - ATTENDS PART OF IMAGE FROM OTHER THREAD AND UPDATE imageArray matrix
 		
 		//4 - CREATE FINAL IMAGE
-		outpam = inpam;
-		outpam.file = fopen("img/imm2.pgm", "w");
-		outpam.plainformat = 1; // Force plain, so that we, useless meatbags, can read what is being written
 
-		
-		//update imageArray with new values
-		//...
-
-		// Write all the resulting values into the file
-		pnm_writepam(&outpam, imageArray);
 
 		// Free space
-		pnm_freepamarray(imageArray, &inpam);
+		pgm_freearray(imageArray, row);
 	} 
 	else 
 	{
@@ -139,7 +114,7 @@ int main(int argc, char *argv[])
 		MPI_Recv(packet,param[0]*param[1],MPI_INT,0,2,MPI_COMM_WORLD,&stat);
 
 		int i,j;
-		printf("RECEIVED: depth=%d rows=%d columsn=%d \n",param[2],param[0],param[1]);
+		printf("THREAD %d: RECEIVED: depth=%d rows=%d columsn=%d \n",my_rank,param[2],param[0],param[1]);
 		for(i=0;i<param[0];i++)
 		{
 			for(j=0;j<param[1];j++)
@@ -149,13 +124,12 @@ int main(int argc, char *argv[])
 				else
 			  		printf("%d ",packet[i][j]);
 			}
-			printf("\n");
+			printf("\n \n");
 		}
-		printf("\n");
-
 
 		//2 - PROCESS IMAGE WITH OPENMPFOR INCREMENT PERFORMANCE
-		printf("CORRECTED\n");
+		/*printf("CORRECTED\n");
+		
 		for(i=0;i<param[0];i++)
 		{
 			for(j=0;j<param[1];j++)
@@ -163,13 +137,13 @@ int main(int argc, char *argv[])
 				int val=applyCorrection(packet[i][j],2,param[2]);
 				packet[i][j]=val;
 				if(packet[i][j]<10)
-		  			printf("%d  ",packet[i][j]);
+		  			printf("%lu  ",packet[i][j]);
 				else
-		  			printf("%d ",packet[i][j]);
+		  			printf("%lu ",packet[i][j]);
 			}
 			printf("\n");
 		}
-
+		*/
 
 		//3 - SEND FINAL PART OF IMAGE TO MASTER THREAD
 		//...
